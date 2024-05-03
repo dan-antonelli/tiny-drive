0a3fcac2c2cc6adfdfa0df32dac636d664ee770b
violettomania <danielpolya@protonmail.com>
2024-05-03 13:13:47 +0200

    add todo, temp disable focus ring

--- src/components/shadcn/ui/checkbox.tsx
+++ src/components/shadcn/ui/checkbox.tsx
@@ -15,7 +15,7 @@ const Checkbox = React.forwardRef<
       className={cn(
         `${
           props.checked && 'mt-3.2'
-        } p-0.5 bg-white peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground`,
+        } p-0.5 bg-white peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50`,
         className
       )}
       {...props}

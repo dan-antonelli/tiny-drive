6a43bea93e09a03404f307160a35d8c0693fd35d
violettomania <danielpolya@protonmail.com>
2024-05-04 20:31:38 +0200

    rounded checkbox with custom css class

--- src/components/shadcn/ui/checkbox.tsx
+++ src/components/shadcn/ui/checkbox.tsx
@@ -13,7 +13,7 @@ const Checkbox = React.forwardRef<
     className={cn(
       `${
         props.checked && 'mt-3.1'
-      } p-0.5 bg-white peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50`,
+      } rounded-border-md p-0.5 bg-white peer h-4 w-4 shrink-0 border border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50`,
       className
     )}
     {...props}

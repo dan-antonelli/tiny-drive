050d23f73f6e7df71cf4251b9b780daa3f524d5d
violettomania <danielpolya@protonmail.com>
2024-05-06 16:30:39 +0200

    add test

---
+++ src/features/table/components/cell/ItemTypeCell.test.tsx
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom';
+
+import { render } from '@testing-library/react';
+
+import ItemTypeCell from './ItemTypeCell';
+
+describe('ItemTypeCell', () => {
+  test('renders FileIcon when type is "file"', () => {
+    const { asFragment } = render(<ItemTypeCell type='file' />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('renders FolderIcon when type is not "file"', () => {
+    const { asFragment } = render(<ItemTypeCell type='folder' />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
